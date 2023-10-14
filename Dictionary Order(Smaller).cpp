83f246df7af5290c1339472db0eb3cdc0da4181a
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-10-14 21:53:49 +0530

    Create Dictionary Order(Smaller).cpp

---
+++ Dictionary Order(Smaller).cpp
@@ -0,0 +1,36 @@
+// https://hack.codingblocks.com/app/contests/4001/1063/problem
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector <string> v;
+void permutation(char arr[], int i){
+    if(arr[i] == '\0'){
+		v.push_back(arr);
+        //cout << arr << endl;
+        return;
+    }
+
+    for ( int j = i; arr[j] != '\0'; j++){
+         swap(arr[j] , arr[i]);
+        permutation(arr,i+1);
+        swap(arr[j] , arr[i]);
+    }
+}
+int main(){
+    char arr[1000];
+    cin.getline(arr,1000);
+
+    permutation(arr,0);
+	sort(v.begin(),v.end());
+	int i;
+	for( i =0; i<v.size(); i++){
+		if(arr == v[i]){
+			break;
+		}
+		cout << v[i] << endl;
+		
+	}
+	
+}
