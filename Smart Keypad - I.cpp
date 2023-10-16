d277601491cdf86724a8dec208a626cef6cb18fe
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-10-16 22:05:57 +0530

    Create Smart Keypad - I.cpp

---
+++ Smart Keypad - I.cpp
@@ -0,0 +1,29 @@
+// https://hack.codingblocks.com/app/contests/4001/742/problem
+
+#include<iostream>
+using namespace std;
+//string arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+string arr[] = { " ", ".+@$", "abc", "def", "ghi", "jkl" , "mno", "pqrs" , "tuv", "wxyz" };
+void phonekeypad(char inp[1000], char out[1000], int i, int j){
+    //base case
+    if(inp[i] == '\0'){
+        out[j] = '\0';
+        cout << out << endl;
+        return;
+    }
+
+    //rec case
+    char ch = inp[i];  //char aya h
+    int index = ch - '0';
+    for (int k = 0; arr[index][k]!= '\0'; k++ ){
+        out [j] = arr[index][k];
+        phonekeypad(inp,out,i+1,j+1);
+    }
+    
+}
+int main(){
+    char inp[1000];
+    cin >> inp;    //23
+    char out[1000];
+    phonekeypad(inp,out,0,0);
+}
