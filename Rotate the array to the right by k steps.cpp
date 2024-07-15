7711d7f376dac458cce1e92f957fa7ccc7814a84
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-07-15 23:44:49 +0530

    Create Rotate the array to the right by k steps.cpp

---
+++ Rotate the array to the right by k steps.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/rotate-array/submissions/1322118476/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+          reverse(nums.begin()+k, nums.end());
+         
+    }
+};
