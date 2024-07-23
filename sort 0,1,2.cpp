35bc0eb4a7511a5cbc9a0878fad38f75f96aa2a4
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-07-23 23:59:40 +0530

    Create sort 0,1,2.cpp

---
+++ sort 0,1,2.cpp
@@ -0,0 +1,18 @@
+ // https://leetcode.com/problems/sort-colors/submissions/1331009224/
+
+int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
