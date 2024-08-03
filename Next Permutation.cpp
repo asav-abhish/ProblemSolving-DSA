d3829172c59697f5aabfbb6f06ab307031d38d51
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-08-03 18:58:50 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,9 @@
+//  https://leetcode.com/problems/next-permutation/description/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+
+    }
+};
