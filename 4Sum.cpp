eaca1b8dfd686ca8629e6afd95e8f54b8094a516
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-08-09 23:57:17 +0530

    Create 4Sum.cpp

---
+++ 4Sum.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/4sum/
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for i
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates for j
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[left], nums[right]};
+                        ans.push_back(temp);
+
+                        while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates for left
+                        while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates for right
+
+                        left++;
+                        right--;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
